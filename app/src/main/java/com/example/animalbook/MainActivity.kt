b1b82672adc5d46712941975d53ddcf86a07c91b
IkezawaYuki <y.ikezawa93@gmail.com>
2019-08-13 23:39:42 +0900

    first commit

---
+++ app/src/main/java/com/example/animalbook/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.animalbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_title.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragment = titleFragment as? TitleFragment
+        fragment?.setTitle("フラグメント図鑑")
+    }
+}
