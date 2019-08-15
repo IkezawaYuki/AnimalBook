74164cfb770477c89ccac09e7ba187f46c567e92
IkezawaYuki <y.ikezawa93@gmail.com>
2019-08-15 23:22:31 +0900

    add fragment file

--- app/src/main/java/com/example/animalbook/SubActivity.kt
+++ app/src/main/java/com/example/animalbook/SubActivity.kt
@@ -2,11 +2,28 @@ package com.example.animalbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub.*
 
 class SubActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+
+        lionButton.setOnClickListener {
+            val fragment = LionFragment()
+            val fragmentManager = this.getSupportFragmentManager()
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragment).addToBackStack(null).commit()
+        }
+
+        hippoButton.setOnClickListener {
+            val fragment = HippoFragment()
+            val fragmentManager = this.getSupportFragmentManager()
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragment).addToBackStack(null).commit()
+        }
+
+        
     }
 }
